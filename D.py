b21005dbd2548c5b94081190002ec9b4c92584a8
Marinakhan <marinakhan384@gmail.com>
2025-07-14 10:32:36 +0500

    Update D.py

--- D.py
+++ D.py
@@ -1314,7 +1314,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/Sourav9106/Tiwari/blob/main/Tiwari").text
+	r1=requests.get(" https://github.com/marinakhan897/Approval/blob/main/Approval.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
